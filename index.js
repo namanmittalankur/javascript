ee1710c0ee8d2a06fac59469a9598178ec8d2262
naman.mittal.ankur@gmail.com <naman.mittal.ankur@gmail.com>
2022-10-26 21:08:57 +0530

    notes for array, function, objects

--- index.js
+++ index.js
@@ -26,16 +26,43 @@ let isApproved = false ; // boolean literal
 //Undefined: It means the value does not exist in the compiler.
 let firstName = undefined;
 let selectedColor = null;
-
-
-
 // end primitive types
 
-function greeet(name, lastname) {
+// start objects
+ // Objects is a variable which has propeties like person has a name and a age. 
+
+let person = {
+    name: 'naman', age: 9
+};
+
+// dot notation
+
+person.name = 'John';
+
+// Bracket notation
+let selection = 'name';
+person[selection] = 'Mary';
+console.log(person.name);
+// end objects
+
+// start-Array
+let selectedColors = ['red', 'blue'];
+selectedColor[2] = 1;
+console.log(selectedColor.length);
+
+// End-Array
+
+// start-functions 
+// Types of functions
+// performing a task
+function greet(name, lastname) {
     console.log('hello ' + name + ' ' + lastname);
 }
 
+greet('john', 'smith');
 
+
+// calculating a value
 function square(number) {
     return number * number;
 }
@@ -43,3 +70,5 @@ result1 = square(5);
 result2 = square(6);
 
 console.log(result2);
+
+// end-functions 
