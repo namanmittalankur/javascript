218dfb7802031e2f6f452389c19b1512ffcc360b
naman.mittal.ankur@gmail.com <naman.mittal.ankur@gmail.com>
2022-10-26 20:38:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//start-Variables
+
+let name = 'Naman';
+console.log(name);
+
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with number (1name)
+// cannot contain space or hypen (-)
+// are case-sensitive
+
+//end-variable
+
+//start const
+
+
+//end const
+function greeet(name, lastname) {
+    console.log('hello ' + name + ' ' + lastname);
+}
+
+
+function square(number) {
+    return number * number;
+}
+result1 = square(5);
+result2 = square(6);
+
+console.log(result2);
