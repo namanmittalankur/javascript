dd979c5f2c3db931fac8a29d00b4728a5a1b3d7a
naman.mittal.ankur@gmail.com <naman.mittal.ankur@gmail.com>
2022-10-28 20:26:59 +0530

    ternary operator

--- index.js
+++ index.js
@@ -131,9 +131,18 @@ console.log(x <= 1); //true
 
 //strict equality (type + value)
 console.log('1' === 1); //false
-console.log(1 = 1);// true
+console.log(1 == 1);// true
 
 //lose equality(value)
 console.log(1 == 1); // true
-console.log('1' = 1);// true
-console.log(true = 1);//true
+console.log('1' == 1);// true
+console.log(true == 1);//true
+
+//ternary operator
+
+//if a customer has more than 100 points, they are 'gold' customer, otherwise, they are 'silver' customer.
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
