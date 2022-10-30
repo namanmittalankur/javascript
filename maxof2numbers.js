c50830541fc0f872cca185894ac0084199391fc1
naman.mittal.ankur@gmail.com <naman.mittal.ankur@gmail.com>
2022-10-30 21:53:20 +0530

    max of 2 num

---
+++ maxof2numbers.js
@@ -0,0 +1,20 @@
+let a = 50;
+let b = 20;
+
+// if (a < b){
+//     console.log(a + ' is small and ' + b + ' is big');
+// }
+// else{
+//     console.log(a + ' is big and '  + b + ' is small');
+// }
+
+if (a < b){
+    console.log(`${a} is small and ${b} is big`);
+}
+else{
+    console.log(`${a} is big and ${b} is small`);
+}
+
+
+
+
